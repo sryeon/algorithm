b46a4a99ff1eb822c9b5cce3ce88708d10c220c8
[디지캡] 연소라 <sryeon@digicaps.com>
2019-01-24 15:00:56 +0900

    sryeon.... 97점 코드

--- jungol_2255_sryeon.cpp
+++ jungol_2255_sryeon.cpp
@@ -1,116 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int EU(int x, int y){
-	int n;
-	while(1){
-		n = x % y;
-		if(n==0) return y;
-		x = y;
-		y = n;
-	}
+ 
+int gcd(int a, int b)
+{
+    return b ? gcd(b, a%b) : a;
 }
-
+int lcm(int a, int b)
+{
+    return a / gcd(a, b)*b;
+}
+ 
 int main (void){
+ 
+    int num;
+    int *ptr_init_array;
+    int *ptr_logic_array;
+    int *ptr_temp_array;
+    int *ptr_lcm_array;
+ 
+    int logic_count = 1;
+	int before_logic_count = 0;
+	int logic_value;
+    int lcm_count = 0;
+	bool lcm_init_flag = false;
 
-	int num;
-	int *ptr_init_array;
-	int *ptr_logic_array;
-	int *ptr_temp_array;
-	int *ptr_lcm_array;
-	int *ptr_lcm_overlap_array;
+    long lcm_value;
+ 
+    bool breakFlag = true;
+    //수열의 갯수를 지정
+    scanf("%d", &num);
+ 
+    if(!(num > 0 && num <= 20000)){
+        printf("INPUT ERROR!");
+        return 0;
+    }
 
-	int logic_count = 1;
-	int lcm_count = 0;
-	long lcm_value;
-	long gcm_value;
+    ptr_init_array = (int *)malloc(num * sizeof(int));
+    ptr_logic_array = (int *)malloc(num * sizeof(int));
+    ptr_temp_array = (int *)malloc(num * sizeof(int));
+    ptr_lcm_array = (int *)malloc(num * sizeof(int));
+     
+    //수열을 입력받는다.
+    for(int i=0;i<num;i++){
+        scanf("%d", &ptr_init_array[i]);
+        ptr_logic_array[i] = i+1; //1부터 증가하는 카드를 배치
+        ptr_lcm_array[i] = 0; // lcm값들 초기화
+    }
+ 
+	lcm_count = 0;
+    while(1){
+ 
+		//수열로직1회 실행. 아래 for문을 돌면, 수열을 통해 카드가 1회 섞임. 즉 수열궤적+1
+        for(int i=0;i<num;i++){
+			// 체크했던 놈이면 다음비교로 넘어감.
+			if(ptr_lcm_array[i] != 0){
+				continue;
+			}
+			
+			// 로직 수행
+			logic_value = ptr_logic_array[ptr_init_array[i] -1];
 
-	bool breakFlag = true;
-	//수열의 갯수를 지정
-	scanf("%d", &num);
+			// 체크한적이 없고 안움직이는 놈이면 체크완료를 처리한 후, 다음비교로 넘어감.
+			if(ptr_logic_array[i] == logic_value){
+				ptr_lcm_array[i] = 1;
+				
+				if(lcm_init_flag == false){
+					lcm_value = 1;
+					lcm_init_flag = true;
+				}
+				lcm_count++;
 
-	if(!(num > 0 && num <= 20000)){
-		printf("INPUT ERROR!");
-		return 0;
-	}
+				if(lcm_count == num){
+					breakFlag = false;
+					break;
+				}
+				continue;
+			}
 
-	ptr_init_array = (int *)malloc(num * sizeof(int));
-	ptr_logic_array = (int *)malloc(num * sizeof(int));
-	ptr_temp_array = (int *)malloc(num * sizeof(int));
-	ptr_lcm_array = (int *)malloc(num * sizeof(int));
-	ptr_lcm_overlap_array = (int *)malloc(num * sizeof(int));
-	
-	//수열을 입력받는다.
-	for(int i=0;i<num;i++){
-		scanf("%d", &ptr_init_array[i]);
-		ptr_logic_array[i] = i+1; //1부터 증가하는 카드를 배치
-		ptr_lcm_array[i] = 0;  // lcm값들 초기화
-	}
+			//로직 저장
+            ptr_temp_array[i] = logic_value;
 
-	while(breakFlag){
+            // 체크한적이 없고 초기값과 일치하는 놈이면?
+            if(ptr_temp_array[i] == i+1){
+                ptr_lcm_array[i] = logic_count;
 
-		for(int i=0;i<num;i++){
-			ptr_temp_array[i] = ptr_logic_array[ptr_init_array[i] -1];
-		}
+				if(lcm_init_flag == false){
+					lcm_value = ptr_lcm_array[i];
+					lcm_init_flag = true;
+				}else if(before_logic_count != logic_count){
+					lcm_value = lcm(lcm_value, ptr_lcm_array[i]);
+					//printf("ptr_lcm_array[%d] : %d / lcm_value : %d\n", i, ptr_lcm_array[i], lcm_value);
+				}
+				lcm_count++;
+
+				if(lcm_count == num){
+					breakFlag = false;
+					break;
+				}
+				//이전 체크횟수를 기억해서, 같은 수는 굳이 LCM로직을 태우지 않음.
+				before_logic_count = logic_count;
+            }
+        }
+
+		if(breakFlag == false)
+			break;
 
 		memcpy(ptr_logic_array, ptr_temp_array, num * sizeof(int));
 
-		lcm_count = 0;
-
-		for(int i=0;i<num;i++){
-			// 초기값과 일치하면서 lcm값이 없던 놈이면?
-			if((ptr_lcm_array[i] == 0) && (ptr_logic_array[i] == i+1)){
-				ptr_lcm_array[i] = logic_count;
-			}
-
-			// lcm값이 모두 할당되었는지 체크.
-			if(ptr_lcm_array[i] != 0){
-				lcm_count++;
-			}
-
-			if(lcm_count == num){
-				breakFlag = false;
-			}
-		}
-
-		logic_count++;
-	}
-	
-	//ptr_lcm_array 중복 제거.
-	int overlap_index = 1;
-	ptr_lcm_overlap_array[0] = ptr_lcm_array[0];
-	for(int i=1;i<num;i++){
-		for(int j=0;j<overlap_index;j++){
-
-			if(ptr_lcm_array[i] == ptr_lcm_overlap_array[j])
-				break;
-
-			if(j == overlap_index -1){
-				ptr_lcm_overlap_array[overlap_index] = ptr_lcm_array[i];
-				overlap_index++;
-			}
-		}
-	}
-
-	//최소 공배수 구하기
-	gcm_value = ptr_lcm_overlap_array[0];
-	lcm_value = ptr_lcm_overlap_array[0];
-
-	for(int i=1;i<overlap_index;i++){
-		gcm_value = EU(gcm_value, ptr_lcm_overlap_array[i]);
-		lcm_value = lcm_value * ptr_lcm_overlap_array[i] / EU(lcm_value, ptr_lcm_overlap_array[i]);
-	}
-	
-	printf("%d\n", lcm_value);
-
-	free(ptr_init_array);
-	free(ptr_logic_array);
-	free(ptr_temp_array);
-	free(ptr_lcm_array);
-	free(ptr_lcm_overlap_array);
-	
-	system("pause");
-
+        logic_count++;
+    }
+     
+    printf("%d\n", lcm_value);
+ 
+    free(ptr_init_array);
+    free(ptr_logic_array);
+    free(ptr_temp_array);
+    free(ptr_lcm_array);
+     
+    system("pause");
+ 
 }
-
